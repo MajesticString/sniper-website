6d404acbbfee88c9ffa8d1b12088c553dd72e2c6
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-11-14 15:15:36 -0800

    Initial commit

---
+++ src/components/navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<mwc-top-app-bar>
+	<img
+		class="sniper-profile"
+		slot="navigationIcon"
+		src="/assets/sniper-profile.png"
+		alt="Sniper profile"
+	/>
+
+	<div slot="title">Sniper</div>
+	<mwc-icon-button class="profile-button" slot="actionItems" icon="person" />
+</mwc-top-app-bar>
+
+<style lang="scss">
+	.sniper-profile {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+	}
+	.profile-button {
+		--mdc-icon-size: 40px;
+	}
+</style>
